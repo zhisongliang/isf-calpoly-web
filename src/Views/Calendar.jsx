36d0b6c990a6beae8d3e55966c870df513401900
ChrisLiang7 <zliang15@calpoly.edu>
2022-12-22 21:14:34 -0800

    fix: dependcy problem

--- src/Views/Calendar.jsx
+++ src/Views/Calendar.jsx
@@ -2,6 +2,40 @@ import { Container } from "react-bootstrap";
 import Fade from 'react-reveal/Fade';
 import { ReactEmbeddedGoogleCalendar } from 'react-embedded-google-calendar';
 
+// const PrivacyButton = () => {
+//   const [showPrivacy, setShowPrivacy] = useState(false);
+  
+//   const handleMouseOver = () => {
+//     setShowPrivacy(true);
+//   }
+
+//   const handleMouseOut = () => {
+//     setShowPrivacy(false);
+//   }
+
+//   return (
+//     <div>
+//       <button 
+//         onMouseOver={handleMouseOver}
+//         onMouseOut={handleMouseOut}
+//         style={{border: 'none', textDecoration: 'underline'}}          
+//         >Privacy Terms
+//       </button>
+//       {showPrivacy && <div style={{textAlign: 'left'}}>
+//       <ul>
+//         <li>If you give your information to us to be invited to our events and programs, you have a right to expect that we will keep your information safe. We respect that. Here's what we do and don't do:</li>
+//         <ul>
+//           <li>Our team (staff, student leaders, and your Friendship Partner) will use your information to invite you to activities, and to get together for meals and social occasions.</li>
+//           <li>We never have and never will share our mailing list with an outside organization for any reason.</li>
+//           <li>We also want you to know that your data is secure with us.</li>
+//           <li>One final note: We are happy to say that we have no connection to any government and just focus on loving international students here. We realize other organizations may use the “ISI” acronym. However, please be assured, International Students, Inc. is not associated with them in any way.</li>
+//         </ul>
+//       </ul>
+// </div>}
+//     </div>
+//   )
+// }
+
 const Calendar = () => {
     return (
         <section id="calendar">
